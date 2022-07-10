727482ae730dae3d052a375b05a9186300a307b7
hezhengmin <zhengmin099@gmail.com>
2022-07-10 22:04:22 +0800

    add

--- Library/Scripts/Account/index.js
+++ Library/Scripts/Account/index.js
@@ -1,9 +1,13 @@
 ﻿import Vue from 'vue';
-
+//import ElementUI from 'element-ui';
+//import 'element-ui/lib/theme-chalk/index.css';
+//import locale from 'element-ui/lib/locale/lang/zh-TW'
+import LoginComponent from './login.vue';
+//Vue.use(ElementUI, { locale })
 
 new Vue({
     el: "#app",
     components: {
-  
+        LoginComponent
     }
 })
--- Library/Scripts/Account/login.vue
+++ Library/Scripts/Account/login.vue
@@ -1,10 +1,24 @@
 ﻿<template>
     <div id="login">
-        <h1>登入</h1>
+        <!--<el-container>
+            <el-header>Header</el-header>
+            <el-main>
+                <el-input placeholder="請輸入帳號"
+                          v-model="accountID"
+                          clearable>
+                </el-input>
+                <el-input placeholder="請輸入密碼" v-model="password" show-password></el-input>
+                <el-button type="primary">登入</el-button>
+            </el-main>
+        </el-container>-->
     </div>
 </template>
 <script>
     export default {
-        name: "login-component"
+        name: "login-component",
+        data: {
+            //accountID: '',
+            //password:''
+        }
     };
 </script>
