4492cc51b35849015eada0c2f81aa0f8c6a6b5ad
hezhengmin <zhengmin099@gmail.com>
2022-07-24 01:19:06 +0800

    add-轉成.NET5

--- Library/Scripts/Account/login.vue
+++ Library/Scripts/Account/login.vue
@@ -1,20 +1,32 @@
 ﻿<template>
     <div id="login">
-        <div class="row bg-light align-items-center w-100">
-            <div class="col-12">
-                <label for="acc" class="form-label">帳號</label>
-                <input id="acc" type="text" class="form-control" v-model="account" />
-            </div>
-            <div class="col-12">
-                <label for="pwd" class="form-label">密碼</label>
-                <input id="pwd" type="password" class="form-control" v-model="password" />
-            </div>
-            <div class="col-12">
-                <div class="d-flex justify-content-center">
-                    <button type="submit" class="btn btn-primary">登入</button>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                </div>
+                <div class="col">
+                    <div class="row bg-light mt-5 rounded">
+                        <div class="col-12 pt-3">
+                            <h3 class="text-center">圖書館-後臺管理系統</h3>
+                            <label for="acc" class="form-label">帳號</label>
+                            <input id="acc" type="text" class="form-control" v-model="account" />
+                        </div>
+                        <div class="col-12 pt-3">
+                            <label for="pwd" class="form-label">密碼</label>
+                            <input id="pwd" type="password" class="form-control" v-model="password" />
+                        </div>
+                        <div class="col-12 p-3">
+                            <div class="d-flex justify-content-center">
+                                <button type="submit" class="btn btn-primary">登入</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
                 </div>
             </div>
         </div>
+       
     </div>
 </template>
 <script>
@@ -31,9 +43,6 @@
 
 <style lang="scss">
     body {
-        align-items: center;
-        padding-top: 40px;
-        padding-bottom: 40px;
         background: url('../../assert/account/img/signin.jpg') no-repeat;
     }
 </style>
