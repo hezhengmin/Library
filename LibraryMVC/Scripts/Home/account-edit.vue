8bb8742008779ee9b7b2cd72a9d4161e686cb2d0
hezhengmin <zhengmin099@gmail.com>
2022-08-28 22:49:59 +0800

    modified account-edit.vue

--- LibraryMVC/Scripts/Home/account-edit.vue
+++ LibraryMVC/Scripts/Home/account-edit.vue
@@ -5,6 +5,7 @@
             電子郵件：<input type="email" v-model="email" required />
             <br />
             <button type="submit">儲存</button>
+            {{$store.state.jwtToken}}
         </form>
         <h1>更改密碼</h1>
         <form @submit.prevent="updatePassword">
@@ -31,7 +32,31 @@
         },
         methods: {
             updateEmail() {
+                let id = '2309E8E9-1E85-4355-B7F1-9055891BB1B6';
+                let url = `https://localhost:44323/api/Account/` + id;
+                let token = this.$store.state.jwtToken;
 
+                console.log(token);
+                this.$axios.patch(url,
+                        [
+                            {
+                                "op": "replace",
+                                "path": "/email",
+                                "value": this.email
+                            }
+                        ]
+                    ,
+                    {
+                        headers: {
+                            Authorization: `Bearer ${token}`
+                        }
+                    })
+                    .then((response) => {
+                        console.log(response.data);
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    })
             },
             //更改密碼
             updatePassword() {
